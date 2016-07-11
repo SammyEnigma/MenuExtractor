c0c354ef8bc3a12d2bfd7342a43984b5db6528c9
Alexander Almaleh <sashoalm@gmail.com>
2016-07-11 20:05:06 +0300

    XmlEnumerator, but write only the QMenu for now.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,29 +1,71 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QXmlStreamWriter>
 
-static void enumerateMenu(QMenu *menu, int level)
+class XmlEnumerator
 {
-    QString s(level*2, ' ');
-    foreach (QObject *ii, menu->findChildren<QObject*>()) {
-        if (QMenu *menu = qobject_cast<QMenu*>(ii)) {
-            qDebug().noquote() << s << menu;
-            enumerateMenu(menu, level+1);
-        } else if (QAction *action = qobject_cast<QAction *>(ii)) {
-            qDebug().noquote() << s << action << (action->isSeparator() ? QString("separator") : action->text());
+public:
+    XmlEnumerator() : writer(&buffer) {
+        writer.setAutoFormatting(true);
+        // The indentation is by 1 in the UI.
+        writer.setAutoFormattingIndent(1);
+    }
+
+    QString xmlOutput() { return buffer; }
+
+    void enumerateMenu(QMenu *menu, int level)
+    {
+        // This is how the XML looks:
+        //
+        // <widget class="QMenu" name="menuFoo">
+        //  <property name="title">
+        //   <string>Foo</string>
+        //  </property>
+        //  ................
+        // </widget>
+
+        // First we write the header of the menu.
+        writer.writeStartElement("widget");
+        writer.writeAttribute("class", "QMenu");
+        writer.writeAttribute("name", menu->objectName());
+        writer.writeStartElement("property");
+        writer.writeAttribute("name", "title");
+        writer.writeTextElement("string", menu->title());
+        writer.writeEndElement();
+
+        // Now write all the nested actions and submenus.
+        foreach (QObject *ii, menu->findChildren<QObject*>()) {
+            if (QMenu *menu = qobject_cast<QMenu*>(ii)) {
+                enumerateMenu(menu, level+1);
+            } else if (QAction *action = qobject_cast<QAction *>(ii)) {
+                // Actions are just referenced here. Their full description is
+                // a top-level xml entry. Top-level means it's a child of the
+                // QMainWindow tag itself. They are outside the scope of the
+                // menu bar widget xml tag.
+
+                // <addaction name="actionBar1"/>
+                writer.writeTextElement("addaction", action->objectName());
+            }
+        }
+
+        // Close the widget XML tag.
+        writer.writeEndElement();
+    }
+
+    // I use a separate function so it can receive a QMenuBar pointer.
+    // Otherwise it's the same code as enumerateMenu().
+    void enumerateMenuBar(QMenuBar *menuBar)
+    {
+        foreach (QMenu *menu, menuBar->findChildren<QMenu*>()) {
+            enumerateMenu(menu, 1);
         }
     }
-}
 
-// I use a separate function so it can receive a QMenuBar pointer.
-// Otherwise it's the same code as enumerateMenu().
-static void enumerateMenuBar(QMenuBar *menuBar)
-{
-    foreach (QMenu *menu, menuBar->findChildren<QMenu*>()) {
-        qDebug() << menu;
-        enumerateMenu(menu, 1);
-    }
-}
+private:
+    QString buffer;
+    QXmlStreamWriter writer;
+};
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -31,7 +73,9 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    enumerateMenuBar(ui->menuBar);
+    XmlEnumerator enumerator;
+    enumerator.enumerateMenuBar(ui->menuBar);
+    qDebug().noquote() << enumerator.xmlOutput();
 }
 
 MainWindow::~MainWindow()
